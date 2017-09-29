8053437d408de950d39ecde7ba2cae583cf3f42b
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2017-09-29 12:18:30 +1000

    More cleanup.

--- simulation/server.py
+++ simulation/server.py
@@ -4,16 +4,6 @@ from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
 from model import HavvenModel
 
-def agent_portrayal(agent):
-    max_wealth = 30
-    frac = min(255, int(255*agent.wealth / max_wealth))
-    color = "#{0:X}{0:X}{1:X}".format(frac, 255 - frac)
-    return {"Shape": "rect",
-            "w": 1.0,
-            "h": 1.0,
-            "Filled": "true",
-            "Color": color,
-            "Layer": 0}
 
 def make_server(n_agents=200, px_width=500, px_height=500):
     charts = [ChartModule([{"Label": "Gini", "Color": "red"},
