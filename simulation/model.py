173b69d884d247a00d620fa4fb2a92a69296f961
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2017-10-22 22:41:58 +1100

    Move DataCollector initialisation into stats.py.

--- simulation/model.py
+++ simulation/model.py
@@ -4,7 +4,6 @@ from decimal import Decimal as Dec
 
 from mesa import Model
 from mesa.time import RandomActivation
-from mesa.datacollection import DataCollector
 
 import stats
 import agents as ag
@@ -16,7 +15,7 @@ from managers import (HavvenManager, MarketManager,
 class Havven(Model):
     """
     An agent-based model of the Havven stablecoin system. This class will
-      provide the basic market functionality of havven, an exchange, and a
+      provide the basic market functionality of Havven, an exchange, and a
       place for the market agents to live and interact.
     The aim is to stabilise the nomin price, but we would also like to measure
       other quantities including liquidity, volatility, wealth concentration,
@@ -27,52 +26,16 @@ class Havven(Model):
                  utilisation_ratio_max: float = 1.0,
                  match_on_order: bool = True) -> None:
 
-        # Mesa setup
+        # Mesa setup.
         super().__init__()
-        self.schedule = RandomActivation(self)
-        self.datacollector = DataCollector(
-            model_reporters={
-                "0": lambda x: 0,  # Note: workaround for showing labels (more info server.py)
-                "1": lambda x: 1,
-                "Nomin Price": lambda h: float(h.market_manager.nomin_fiat_market.price),
-                "Nomin Ask": lambda h: float(h.market_manager.nomin_fiat_market.lowest_ask_price()),
-                "Nomin Bid": lambda h: float(h.market_manager.nomin_fiat_market.highest_bid_price()),
-                "Curit Price": lambda h: float(h.market_manager.curit_fiat_market.price),
-                "Curit Ask": lambda h: float(h.market_manager.curit_fiat_market.lowest_ask_price()),
-                "Curit Bid": lambda h: float(h.market_manager.curit_fiat_market.highest_bid_price()),
-                "Curit/Nomin Price": lambda h: float(h.market_manager.curit_nomin_market.price),
-                "Curit/Nomin Ask": lambda h: float(h.market_manager.curit_nomin_market.lowest_ask_price()),
-                "Curit/Nomin Bid": lambda h: float(h.market_manager.curit_nomin_market.highest_bid_price()),
-                "Havven Nomins": lambda h: float(h.manager.nomins),
-                "Havven Curits": lambda h: float(h.manager.curits),
-                "Havven Fiat": lambda h: float(h.manager.fiat),
-                "Gini": stats.gini,
-                "Nomins": lambda h: float(h.manager.nomin_supply),
-                "Escrowed Curits": lambda h: float(h.manager.escrowed_curits),
-                #"Wealth SD": stats.wealth_sd,
-                "Max Wealth": stats.max_wealth,
-                "Min Wealth": stats.min_wealth,
-                "Avg Profit %": lambda h: round(100*stats.mean_profit_fraction(h), 3),
-                "Bank Profit %": lambda h: round(100*stats.mean_banker_profit_fraction(h), 3),
-                "Arb Profit %": lambda h: round(100*stats.mean_arb_profit_fraction(h), 3),
-                "Rand Profit %": lambda h: round(100*stats.mean_rand_profit_fraction(h), 3),
-                "NomShort Profit %": lambda h: round(100*stats.mean_nomshort_profit_fraction(h), 3),
-                "Curit Demand": stats.curit_demand,
-                "Curit Supply": stats.curit_supply,
-                "Nomin Demand": stats.nomin_demand,
-                "Nomin Supply": stats.nomin_supply,
-                "Fiat Demand": stats.fiat_demand,
-                "Fiat Supply": stats.fiat_supply,
-                "Fee Pool": lambda h: float(h.manager.nomins),
-                "Fees Distributed": lambda h: float(h.fee_manager.fees_distributed),
-                "NominFiatOrderBook": lambda h: h.market_manager.nomin_fiat_market,
-                "CuritFiatOrderBook": lambda h: h.market_manager.curit_fiat_market,
-                "CuritNominOrderBook": lambda h: h.market_manager.curit_nomin_market
-            }, agent_reporters={
-                "Agents": lambda a: a,
-            })
 
-        # Initiate time itself.
+        # The schedule will activate agents in a random order per step.
+        self.schedule = RandomActivation(self)
+
+        # Set up data collection.
+        self.datacollector = stats.create_datacollector()
+
+        # Initiate Time Itself.
         self.time: int = 0
 
         # Initialise simulation managers.
@@ -90,8 +53,8 @@ class Havven(Model):
         self.agent_manager = AgentManager(self, num_agents,
                                           fractions, Dec(init_value))
 
-    def fiat_value(self, curits: Dec = Dec('0'), nomins: Dec = Dec('0'),
-                   fiat: Dec = Dec('0')) -> Dec:
+    def fiat_value(self, curits = Dec('0'), nomins = Dec('0'),
+                   fiat = Dec('0')) -> Dec:
         """Return the equivalent fiat value of the given currency basket."""
         return self.market_manager.curits_to_fiat(curits) + \
             self.market_manager.nomins_to_fiat(nomins) + fiat
@@ -105,10 +68,10 @@ class Havven(Model):
 
     def step(self) -> None:
         """Advance the model by one step."""
-        # Agents submit trades
+        # Agents submit trades>
         self.schedule.step()
 
-        # Resolve outstanding trades
+        # Resolve outstanding trades.
         if not self.manager.match_on_order:
             self.market_manager.curit_nomin_market.match()
             self.market_manager.curit_fiat_market.match()
@@ -118,7 +81,8 @@ class Havven(Model):
         if (self.time % self.fee_manager.fee_period) == 0:
             self.fee_manager.distribute_fees(self.schedule.agents)
 
-        # Collect data
+        # Collect data.
         self.datacollector.collect(self)
 
+        # Advance Time Itself.
         self.time += 1
--- simulation/stats.py
+++ simulation/stats.py
@@ -2,6 +2,8 @@
 
 from statistics import mean, stdev
 
+from mesa.datacollection import DataCollector
+
 import model
 import agents
 
@@ -96,3 +98,47 @@ def nomin_supply(havven: "model.Havven") -> float:
     curits = sum([float(bid.quantity * bid.price) for bid in havven.market_manager.curit_nomin_market.bids])
     fiat = sum([float(ask.quantity) for ask in havven.market_manager.nomin_fiat_market.asks])
     return curits + fiat
+
+
+def create_datacollector() -> DataCollector:
+    return DataCollector(
+                model_reporters={
+                    "0": lambda x: 0,  # Note: workaround for showing labels (more info server.py)
+                    "1": lambda x: 1,
+                    "Nomin Price": lambda h: float(h.market_manager.nomin_fiat_market.price),
+                    "Nomin Ask": lambda h: float(h.market_manager.nomin_fiat_market.lowest_ask_price()),
+                    "Nomin Bid": lambda h: float(h.market_manager.nomin_fiat_market.highest_bid_price()),
+                    "Curit Price": lambda h: float(h.market_manager.curit_fiat_market.price),
+                    "Curit Ask": lambda h: float(h.market_manager.curit_fiat_market.lowest_ask_price()),
+                    "Curit Bid": lambda h: float(h.market_manager.curit_fiat_market.highest_bid_price()),
+                    "Curit/Nomin Price": lambda h: float(h.market_manager.curit_nomin_market.price),
+                    "Curit/Nomin Ask": lambda h: float(h.market_manager.curit_nomin_market.lowest_ask_price()),
+                    "Curit/Nomin Bid": lambda h: float(h.market_manager.curit_nomin_market.highest_bid_price()),
+                    "Havven Nomins": lambda h: float(h.manager.nomins),
+                    "Havven Curits": lambda h: float(h.manager.curits),
+                    "Havven Fiat": lambda h: float(h.manager.fiat),
+                    "Gini": gini,
+                    "Nomins": lambda h: float(h.manager.nomin_supply),
+                    "Escrowed Curits": lambda h: float(h.manager.escrowed_curits),
+                    #"Wealth SD": stats.wealth_sd,
+                    "Max Wealth": max_wealth,
+                    "Min Wealth": min_wealth,
+                    "Avg Profit %": lambda h: round(100 * mean_profit_fraction(h), 3),
+                    "Bank Profit %": lambda h: round(100 * mean_banker_profit_fraction(h), 3),
+                    "Arb Profit %": lambda h: round(100 * mean_arb_profit_fraction(h), 3),
+                    "Rand Profit %": lambda h: round(100 * mean_rand_profit_fraction(h), 3),
+                    "NomShort Profit %": lambda h: round(100 * mean_nomshort_profit_fraction(h), 3),
+                    "Curit Demand": curit_demand,
+                    "Curit Supply": curit_supply,
+                    "Nomin Demand": nomin_demand,
+                    "Nomin Supply": nomin_supply,
+                    "Fiat Demand": fiat_demand,
+                    "Fiat Supply": fiat_supply,
+                    "Fee Pool": lambda h: float(h.manager.nomins),
+                    "Fees Distributed": lambda h: float(h.fee_manager.fees_distributed),
+                    "NominFiatOrderBook": lambda h: h.market_manager.nomin_fiat_market,
+                    "CuritFiatOrderBook": lambda h: h.market_manager.curit_fiat_market,
+                    "CuritNominOrderBook": lambda h: h.market_manager.curit_nomin_market
+                }, agent_reporters={
+                    "Agents": lambda a: a,
+                })
