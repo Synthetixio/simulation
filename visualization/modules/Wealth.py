6876262bad0f121bd7ce88f874831c5a0cc8280d
DominicRomanowski <romanowski.dominic@gmail.com>
2017-11-13 09:48:11 +1100

    need to add trade history to players, brb

--- visualization/modules/Wealth.py
+++ visualization/modules/Wealth.py
@@ -150,3 +150,64 @@ class CurrentOrderModule(BarGraphModule):
         except Exception:
             vals = []
         return vals
+
+
+class PastOrdersModule(BarGraphModule):
+    def render(self, model: Havven) -> OrderbookValueTuple:
+        data_collector: "DataCollector" = getattr(
+            model, self.data_collector_name
+        )
+
+        # vals are [datasets],[colours],[bar #],[playername],[dataset 1],...[dataset n]
+
+        vals: OrderbookValueTuple = (
+            ["NomFiatAsk",  "NomFiatBid", "CurFiatAsk", "CurFiatBid", "CurNomAsk", "CurNomBid"],
+            ["deepskyblue", "#179473",    "red",        "#8C2E00",    "purple",    "#995266"],
+            [1, 1, 2, 2, 3, 3], [], [], [], [], [], [], []
+        )
+
+        try:
+            agents = sorted(
+                data_collector.agent_vars["Agents"][-1],
+                key=lambda x: x[0]
+            )[:-1]
+
+            for item in agents:
+                vals[3].append(item[1].name)
+                orders = item[1].trades
+                nom_fiat_ask_tot = 0
+                nom_fiat_bid_tot = 0
+                cur_fiat_ask_tot = 0
+                cur_fiat_bid_tot = 0
+                nom_cur_ask_tot = 0
+                nom_cur_bid_tot = 0
+
+                for order in orders:
+                    if order.book.quote == "fiat":
+                        if order.book.base == "nomins":
+                            # FIAT/NOM
+                            if type(order) == Ask:
+                                nom_fiat_ask_tot += order.quantity
+                            if type(order) == Bid:
+                                nom_fiat_bid_tot += order.quantity*order.price
+                        if order.book.base == "curits":
+                            if type(order) == Ask:
+                                cur_fiat_ask_tot += order.quantity
+                            if type(order) == Bid:
+                                cur_fiat_bid_tot += order.quantity*order.price
+                    elif order.book.quote == "nomins":
+                        if type(order) == Ask:
+                            nom_cur_ask_tot += order.quantity
+                        if type(order) == Bid:
+                            nom_cur_bid_tot += order.quantity*order.price
+
+                vals[4].append(float(nom_fiat_ask_tot))
+                vals[5].append(-float(nom_fiat_bid_tot))
+                vals[6].append(float(cur_fiat_ask_tot))
+                vals[7].append(-float(cur_fiat_bid_tot))
+                vals[8].append(float(nom_cur_ask_tot))
+                vals[9].append(-float(nom_cur_bid_tot))
+
+        except Exception:
+            vals = []
+        return vals
