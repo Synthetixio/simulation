0941fb7715f280b453bb6d8a9db749ad17268c13
DominicRomanowski <romanowski.dominic@gmail.com>
2017-11-24 12:58:41 +1100

    turned threaded off, it seems to be slower than having it on...

--- run.py
+++ run.py
@@ -2,6 +2,6 @@
 from mesa.visualization.ModularVisualization import ModularServer
 import server
 
-S: ModularServer = server.make_server(threaded=True)
+S: ModularServer = server.make_server(threaded=False)
 S.port = 3000
 S.launch()
